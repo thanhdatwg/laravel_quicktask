d6b9a075f1cd2c2758d32ad3785aa2de923f38b3
thanhdat <thanhdatwg@gmail.com>
2021-08-16 22:05:01 +0700

    create interface home page, blog page and build pots table structure

---
+++ LaravelQuicktask/database/migrations/2021_08_14_043730_posts.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    //create posts table
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug');
+            $table->string('title');
+            $table->longText('description');
+            $table->string('image_path');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    //delete posts table
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
--- LaravelQuicktask/resources/views/auth/login.blade.php
+++ LaravelQuicktask/resources/views/auth/login.blade.php
@@ -82,3 +82,4 @@
     </div>
 </main>
 @endsection
+
